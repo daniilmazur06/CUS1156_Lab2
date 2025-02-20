998e42cee4cfbd17a8a841ab0de1b46e34b6b1f7
Daniil <daniil.mazur23@my.stjohns.edu>
2025-02-20 15:56:55 -0500

    Add unique counting logic

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,37 +1,34 @@
-import java.util.ArrayList;
+
+/* Project: Lab 2
+* Class: UniqueWords.java
+* Author: Daniil Mazur
+* Date: February 21
+* This program counts how many unique words there are in the string list
+*/
+
+import java.util.*;
 
 public class UniqueWords
 {
-   /**
-		counts the number of unique strings in a list
-		@param list ArrayList of strings to be examined
-		@return number of unique strings in the list
-   */
-   public static int countUnique(ArrayList<String> list)
-   {
-	  int count = 0;
-	  
-      for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
-      }
-	  return count;
-   }
 
-   public static void main(String[] args)
-   {
-      ArrayList <String> words = new ArrayList<>();
-      words.add("apple");
-	  words.add("orange");
-	  words.add("blackboard");
-	  words.add("apple");
-	  words.add("orange");
-	  words.add("sun");
-	  words.add("moon");
-   
-	  int unique = countUnique(words);
-      System.out.println(words + " has " + unique + " unique words");
-   }
+	
+	public static int countUnique(ArrayList<String> list) {
+        Set<String> uniqueSet = new HashSet<>(list);
+        return uniqueSet.size();
+    }
+
+	
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<>();
+        words.add("apple");
+        words.add("orange");
+        words.add("blackboard");
+        words.add("apple");
+        words.add("orange");
+        words.add("sun");
+        words.add("moon");
+
+        int unique = countUnique(words);
+        System.out.println(words + " has " + unique + " unique words");
+    }
 }
